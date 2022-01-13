b93b298902b3e2ada5e17d34fa217195bfb4f894
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-01-13 06:57:21 -0600

    Update appveyor-win.sh

--- scripts/appveyor-win.sh
+++ scripts/appveyor-win.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "APPVEYOR_BUILD_FOLDER=$APPVEYOR_BUILD_FOLDER"
+echo "APPVEYOR_BUILD_FOLDER=$GITHUB_WORKSPACE"
 
 mkdir ninja && cd ninja || exit 1
-curl https://github.com/ninja-build/ninja/releases/download/v1.9.0/ninja-win.zip && 7z x ninja-win.zip || exit 1
+curl https://github.com/ninja-build/ninja/releases/download/v1.9.0/ninja-win.zip && ls && 7z x ninja-win.zip || exit 1
 cd .. || exit 1
 
 mkdir yasm && cd yasm || exit 1
